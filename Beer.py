29505b5f5913fd49b17eb009652d9ec991437366
Luther Richardson <lazrichardson@users.noreply.github.com>
2020-02-20 14:03:18 -0500

    updates

--- Beer.py
+++ Beer.py
@@ -1,4 +1,3 @@
-# TODO: Beer must be imported by the program
 # TODO: at least 1 private and 2 public self attributes
 # TODO: at least 1 private and 1 public method that take arguments, return values and are used by your program
 
@@ -7,21 +6,16 @@ from datetime import date
 
 class Beer:  # Requirement: User-defined class.
 
-    def __init__(self, name, brewer, brewer_location, rating, style):
-        self.name = name
-        self.brewer = brewer
-        self.brewer_location = brewer_location
-        self.rating = rating
-        self.style = style
+    def __init__(self):
+        self.name = input("Input a beer name: ")
+        self.rating = input("Input your rating: ")
+        self.style = input("Input the beer style: ")
+        self.brewer = input("Input the name of the brewer: ")
+        self.brewer_location = input("Input the location of the brewer: ")
         self.input_date = date.today()
 
     def __repr__(self):  # Requirement: implement repr() method
-        return repr(self.name + self.brewer + self.brewer_location + self.rating + self.style + self.input_date)
-
-
-# TODO: Data import of each beer
-def beer_input():
-    print("Not implemented")
+        return repr(self.name + self.brewer + self.brewer_location + self.rating + self.style + str(self.input_date))
 
 
 # TODO: Input data via CSV
@@ -30,20 +24,17 @@ def import_data(file_path):
 
 
 # TODO: Export my data from the tracker via CSV
-def export_data(file_path):
+def export_data():
     print("Not implemented")
 
 
-# TODO: top beers by rating
-def top_beers_by_rating():
-    print("Not implemented")
-
-
-# TODO: top beers by type
-def top_beers_by_type():
-    print("Not implemented")
-
-
-# TODO: search for beers by name
-def find_beer_by_name():
-    print("Not implemented")
+def top_score(beer_list, beer_type=None, beer_name=None):
+    if beer_type is not None:  # TODO: top beers by type
+        print("not implemented")
+    if beer_name is not None:  # TODO: search for beers by name
+        print("not implemented")
+    else:  # TODO: top beers by rating
+        top_scoring_beer = beer_list[0]
+        for beer in beer_list:
+            if beer.rating > top_scoring_beer.rating:
+                top_scoring_beer = beer
--- final_project.py
+++ final_project.py
@@ -8,6 +8,20 @@ Command line beer tracker
 - Try blocks
 TODO: Provide unit tests that prove that your class methods work; should evaluate results using assert statements.
 """
-beer_list = []  # store the list of beers
+import Beer
 
+beer_list = []
+escape = "E"
+user_input = ""
 
+while user_input != escape:
+    user_input = input("Input 1 to input a new beer: ")
+
+    if user_input == '1':
+        beer_list.append(Beer.Beer())
+        print("New beer added...")
+    elif user_input == "E":
+        print("Goodbye!...")
+        break
+
+print(beer_list)
